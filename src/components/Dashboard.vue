c14c2750b1af5fcd7a7e823adead52985471773f
Joseph <jrenderle13@gmail.com>
2023-12-22 19:46:44 -0500

    add initial population of states via store

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,124 +1,157 @@
 <template>
-    <p>Velocity X: {{ ship.velocity.x!.toFixed(2) }}</p>
-    <p>Velocity Y: {{ ship.velocity.y!.toFixed(2) }}</p>
-    <p>Accleration X: {{ ship.acceleration.x!.toFixed(2) }} Accleration Y: {{ ship.acceleration.y?.toFixed(2) }}</p>
-    <p>Angle: {{ (ship.rotationAngle * (180 / Math.PI)).toFixed(0) }}</p>
-    <div class="speed-controls">
+  <p>Velocity X: {{ currentScenario.ship.velocity.x!.toFixed(2) }}</p>
+  <p>Velocity Y: {{ currentScenario.ship.velocity.y!.toFixed(2) }}</p>
+  <p>
+    Accleration X: {{ currentScenario.ship.acceleration.x!.toFixed(2) }} Accleration Y:
+    {{ currentScenario.ship.acceleration.y?.toFixed(2) }}
+  </p>
+  <p>Angle: {{ (currentScenario.ship.rotationAngle * (180 / Math.PI)).toFixed(0) }}</p>
+  <div class="speed-controls">
     <input type="range" :min="1" :max="300" :step="1" v-model="speed" />
     <span>{{ speed }}</span>
     <button @click="handlePause">Pause</button>
   </div>
-    <!-- <div>{{ keysPressed }}</div> -->
-    <p>Position: {{ ship.position.x!.toFixed(2) }}, {{ ship.position.y!.toFixed(2) }}</p>
-    <div class="canvas-container">
-      <CanvasWithControls :magnificationOpts="{
+  <p>
+    Position: {{ currentScenario.ship.position.x!.toFixed(2) }},
+    {{ currentScenario.ship.position.y!.toFixed(2) }}
+  </p>
+  <div class="canvas-container">
+    <CanvasWithControls
+      :magnificationOpts="{
         stepValue: 1,
         minValue: 1,
         maxValue: 10000,
         defaultMagnification: 6000,
-
-      }" :ship="ship"  :other-objects="[otherObject]" :background="true"></CanvasWithControls>
-      <CanvasWithControls :magnificationOpts="{
+      }"
+      :ship="currentScenario.ship"
+      :other-objects="currentScenario.otherBodies"
+      :background="true"
+    ></CanvasWithControls>
+    <CanvasWithControls
+      :magnificationOpts="{
         stepValue: 1,
         minValue: 1,
         maxValue: 1000000,
         defaultMagnification: 407143,
       }"
-      :ship="ship" :other-objects="[otherObject]" :background="false" :canvas-size="canvasSize"></CanvasWithControls>
+      :ship="currentScenario.ship"
+      :other-objects="currentScenario.otherBodies"
+      :background="false"
+      :canvas-size="canvasSize"
+    ></CanvasWithControls>
   </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { onMounted, onUnmounted, ref, computed, watch } from 'vue';
-  import { useKeyPress } from '../composables/useKeyPress';
-  import { Ship } from '../entitites/ship';
-  import { Planet } from '../entitites/planet';
-import { GameEngine } from '../entitites/GameEngine';
-  import CanvasWithControls from './CanvasWithControls.vue';
-  import { useMainStore } from '../store/store';
-import { Physics } from '../entitites/physics';
-  
-  const { keysPressed, onKeydown, onKeyup } = useKeyPress();
-  const massStation = 420000;
-  const G = 6.674 * Math.pow(10, -11); 
-  const earthMass = 5.972e+24;
-  const distanceFromCenterOfEarth = 6_791_000;
-  const speed = ref(1);
-  const orbitalVelocity = Math.sqrt(G * earthMass / distanceFromCenterOfEarth);
-  const ship = ref(new Ship('ship', massStation, { x: 0, y: 0 }, { x:  -orbitalVelocity, y: 0 }, 100));
-  const earthRadius = 6_371_000;
-  const otherObject = ref(new Planet({ x: 0, y: distanceFromCenterOfEarth }, earthMass, { x: 0, y: 0 }, earthRadius, 'earth'));
-  const mainStore = useMainStore();
-  const gameEngine = new GameEngine(ship.value, [otherObject.value], keysPressed, speed, new Physics());
-  const canvasSize = computed(() => ({
-  x: 250, 
-  y: 250
-    }));
+</template>
 
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref, computed, watch } from "vue";
+import { useKeyPress } from "../composables/useKeyPress";
+import { Ship } from "../entitites/ship";
+import { GameEngine } from "../entitites/GameEngine";
+import CanvasWithControls from "./CanvasWithControls.vue";
+import { useMainStore } from "../store/store";
+import { Physics } from "../entitites/physics";
 
-watch(() => ship.value.firingThruster, (newVal, oldVal) => {
-  if (newVal !== false && oldVal !== true) {
-    return;
-  }
-  mainStore.setTrajectoryData([]);
-  const position = { ...ship.value.position };
-  const velocity = { ...ship.value.velocity };
-  const acceleration = { ...ship.value.acceleration };
-  const otherMapped = [otherObject.value].map(({ mass, position, acceleration, velocity}) => {
-    return {
-      mass, 
-      position: { ...position },
-      acceleration: { ...acceleration },
-      velocity: { ...velocity },
+const { keysPressed, onKeydown, onKeyup } = useKeyPress();
+const massStation = 420000;
+const G = 6.674 * Math.pow(10, -11);
+const earthMass = 5.972e24;
+const distanceFromCenterOfEarth = 6_791_000;
+const speed = ref(1);
+const orbitalVelocity = Math.sqrt((G * earthMass) / distanceFromCenterOfEarth);
+const ship = ref(
+  new Ship(
+    "ship",
+    massStation,
+    { x: 0, y: 0 },
+    { x: -orbitalVelocity, y: 0 },
+    100
+  )
+);
+const currentScenario = computed(() => {
+  return mainStore.initialState;
+})
+const mainStore = useMainStore();
+const gameEngine = new GameEngine(
+  currentScenario.value.ship,
+  currentScenario.value.otherBodies,
+  keysPressed,
+  speed,
+  new Physics()
+);
+const canvasSize = computed(() => ({
+  x: 250,
+  y: 250,
+}));
+
+watch(
+  () => ship.value.firingThruster,
+  (newVal, oldVal) => {
+    if (newVal !== false && oldVal !== true) {
+      return;
     }
-  })
-  worker.postMessage({
-    shipData: {
-      position,
-      velocity,
-      acceleration,
-      mass: ship.value.mass,
-    },
-    timeStep: Number(speed.value),
-    otherBodies: otherMapped,
-    window: [0, 3000],
-  },
-  );
-});
+    mainStore.setTrajectoryData([]);
+    const position = { ...currentScenario.value.ship.position };
+    const velocity = { ...currentScenario.value.ship.velocity };
+    const acceleration = { ...currentScenario.value.ship.acceleration };
+    const otherMapped = currentScenario.value.otherBodies.map(
+      ({ mass, position, acceleration, velocity }) => {
+        return {
+          mass,
+          position: { ...position },
+          acceleration: { ...acceleration },
+          velocity: { ...velocity },
+        };
+      }
+    );
+    worker.postMessage({
+      shipData: {
+        position,
+        velocity,
+        acceleration,
+        mass: currentScenario.value.ship.mass,
+      },
+      timeStep: Number(speed.value),
+      otherBodies: otherMapped,
+      window: [0, 3000],
+    });
+  }
+);
 let worker: Worker;
-  onMounted(() => {
-    gameEngine.start();
-   worker = new Worker(new URL('../workers/trajectoryWorker.ts', import.meta.url), { type: 'module' });
-        window.addEventListener('keydown', onKeydown);
-    window.addEventListener('keyup', onKeyup);
-    worker.onmessage = (event) => {
-  const { chunk } = event.data;
-  mainStore.setTrajectoryData([...mainStore.trajectoryData, ...chunk]);
-      };
-worker.onerror = (error) => {
-  console.error('Worker error:', error);
-};
-  });
-  
-  onUnmounted(() => {
-    worker.terminate();
-    window.removeEventListener('keydown', onKeydown);
-    window.removeEventListener('keyup', onKeyup);
-    gameEngine.stop();
-  });
-  function handlePause() {
-    mainStore.setPause();
-    if (mainStore.pause) {
-      speed.value = 0;
-    } else {
-      speed.value = 1;
-    }
+onMounted(() => {
+  gameEngine.start();
+  worker = new Worker(
+    new URL("../workers/trajectoryWorker.ts", import.meta.url),
+    { type: "module" }
+  );
+  window.addEventListener("keydown", onKeydown);
+  window.addEventListener("keyup", onKeyup);
+  worker.onmessage = (event) => {
+    const { chunk } = event.data;
+    mainStore.setTrajectoryData([...mainStore.trajectoryData, ...chunk]);
+  };
+  worker.onerror = (error) => {
+    console.error("Worker error:", error);
+  };
+});
+
+onUnmounted(() => {
+  worker.terminate();
+  window.removeEventListener("keydown", onKeydown);
+  window.removeEventListener("keyup", onKeyup);
+  gameEngine.stop();
+});
+function handlePause() {
+  mainStore.setPause();
+  if (mainStore.pause) {
+    speed.value = 0;
+  } else {
+    speed.value = 1;
   }
-  </script>
-  <style>
-  .canvas-container {
-    display: flex;
-    flex-direction: row;
-  }
+}
+</script>
+<style>
+.canvas-container {
+  display: flex;
+  flex-direction: row;
+}
 </style>
-  
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,29 @@
 import { defineStore } from 'pinia';
 import { Vector2D } from '../interfaces';
+import { Ship } from '../entitites/ship';
+import { Planet } from '../entitites/planet';
+const earthRadius = 6_371_000;
+const massStation = 420000;
+const distanceFromCenterOfEarth = 6_791_000;
+const earthMass = 5.972e+24;
+
+const initialState = [
+  {
+    id: 1,
+    name: 'Planet Earth',
+    ship: new Ship('ship', massStation, { x: 0, y: 0 }, { x: -7661.011873789845, y: 0 }, 100),
+    otherBodies: [
+      new Planet({ x: 0, y: distanceFromCenterOfEarth }, earthMass, { x: 0, y: 0 }, earthRadius, 'earth'),
+    ]
+  },
+  {
+    id: 1,
+    name: 'Empty Space',
+    ship: new Ship('ship', massStation, { x: 0, y: 0 }, { x: 0, y: 0 }, 100),
+    otherBodies: []
+  }
+]
+
 
 export const useMainStore = defineStore('main', {
   state: () => ({
@@ -7,6 +31,7 @@ export const useMainStore = defineStore('main', {
     loading: false,
     error: null,
     pause: false,
+    initialState: initialState[0],
   }),
   actions: {
     setTrajectoryData(data: Vector2D[]) {
