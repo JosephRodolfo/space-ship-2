f165fda5be7e12ce699f877b44a04007e2da1ff8
Joseph <jrenderle13@gmail.com>
2024-05-12 11:31:30 -0400

    add speedslider component

---
+++ src/components/SpeedSlider.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="button speed-controls">
+    <div class="button">
+      <label for="speed">Speed: </label>
+      <input
+        type="number"
+        id="speed"
+        :min="speedSettings.min"
+        :max="speedSettings.max"
+        :step="currentStep"
+        :value="mainStore.gameEngine.speed"
+        @input="setSpeed($event)"
+      />
+    </div>
+    <div class="button">
+      <label for="step-size">Step Size: </label>
+      <input
+        id="step-size"
+        type="number"
+        :value="settings.stepSize"
+        @input="updateStepSize($event)"
+        min="1"
+      />
+    </div>
+    <div class="button">
+      <label for="logarithmic-toggle">Logarithmic:</label>
+      <input id="logarithmic-toggle" type="checkbox" v-model="settings.logarithmic" />
+    </div>
+    <div class="button">
+      <label for="logarithmic-sensitivity">Logarithmic Sensitivity: </label>
+      <input
+        id="logarithmic-sensitivity"
+        type="number"
+        v-model="settings.logarithmicSensitivity"
+        min="1"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, reactive } from "vue";
+import { useMainStore } from "../store/store";
+
+const mainStore = useMainStore();
+const currentScenario = computed(() => mainStore.initialState);
+const speedSettings = computed(() => currentScenario.value!.speedSettings);
+const settings = reactive({
+  stepSize: 1,
+  logarithmicSensitivity: 1,
+  logarithmic: false,
+});
+
+const currentStep = computed(() => {
+    if (settings.stepSize) {
+    const baseLog = Math.log10(mainStore.gameEngine.speed);
+    const adjustedLog = baseLog / settings.logarithmicSensitivity;
+    return Math.pow(10, Math.floor(adjustedLog));
+  }
+    return settings.stepSize;
+});
+
+
+function setSpeed(event: Event) {
+  const inputElement = event.target as HTMLInputElement;
+  mainStore.setSpeed(Number(inputElement.value));
+}
+
+function updateStepSize(event: Event) {
+  const inputElement = event.target as HTMLInputElement;
+  const numValue = Math.max(Number(inputElement.value), 1);
+  settings.stepSize = numValue;
+}
+</script>
+
+<style scoped>
+input[type="range"] {
+  margin: 10px 0;
+}
+</style>
